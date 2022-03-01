4b7a13db4f7033aecc29f5d72c7b8e5daa239d10
holmech-dev <tsch@leeds.ac.uk>
2022-03-01 14:35:41 +0000

    base dockerfile update for cuda-11.6

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nvidia-docker build -t holmech:kinetic-base -f Dockerfile.base .
-nvidia-docker build -t holmech:kinetic-cuda -f Dockerfile.cuda .
-nvidia-docker build -t holmech:kinetic-latest -f Dockerfile.kinetic .
+nvidia-docker build -t holmech:focal-cuda-base -f Dockerfile.base .
+#nvidia-docker build -t holmech:kinetic-cuda -f Dockerfile.cuda .
+#nvidia-docker build -t holmech:kinetic-latest -f Dockerfile.kinetic .
--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ nvidia-docker run -it --rm\
     --volume="$ROS_DATA_DIR:/ROS_DATA_DIR:rw"\
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="$HOME:/home/chris:rw" \
-    --workdir="/home/chris/holmech-dev/pedestrian_interaction/Pedestrian-Interactions-ws" \
-    --name="Kinetic-latest-test_0" \
+    --workdir="/home/chris/_holmech-dev" \
+    --name="focal-cuda-test_0" \
     --privileged=True \
-    holmech:kinetic-latest
+    holmech:focal-cuda-base
