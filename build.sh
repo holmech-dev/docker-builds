0b2c195018da501891a96ee7d7cb468dd38ed28a
Chris Holmes <tsch@leeds.ac.uk>
2023-11-30 22:04:08 +0000

    new branch

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nvidia-docker build -t holmech:kinetic-base -f Dockerfile.base .
-nvidia-docker build -t holmech:kinetic-cuda -f Dockerfile.cuda .
-nvidia-docker build -t holmech:kinetic-latest -f Dockerfile.kinetic .
+#nvidia-docker build -t holmec/kinetic-base -f Dockerfile.base .
+#nvidia-docker build -t holmec/kinetic-cuda -f Dockerfile.cuda .
+nvidia-docker build -t holmec/kinetic-latest -f Dockerfile.kinetic .
--- run.sh
+++ run.sh
@@ -8,15 +8,15 @@ xhost +
 #swd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 nvidia-docker run -it --rm\
+    --runtime=nvidia \
     --net=host \
     --env="DISPLAY" \
     --env=TERM="xterm-color" \
     --env=HOME="/home/chris" \
     --env="QT_X11_NO_MITSHM=1" \
-    --volume="$ROS_DATA_DIR:/ROS_DATA_DIR:rw"\
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="$HOME:/home/chris:rw" \
-    --workdir="/home/chris/holmech-dev/pedestrian_interaction/Pedestrian-Interactions-ws" \
+    --workdir="/home/chris/projects/" \
     --name="Kinetic-latest-test_0" \
     --privileged=True \
-    holmech:kinetic-latest
+    holmec/kinetic-base
